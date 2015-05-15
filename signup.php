c5103d1e8caa1b5d932547799b260a48c0494d8e
LexusMans <lexus_mans@MacBookPro-C82A1425C4EC.local>
2015-05-15 08:28:28 -0500

    added signup page

---
+++ signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<style type="text/css">
+   		body { background: navy; }
+	</style>
+
+  <head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+  	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+	<?php include_once("template_navbar.php"); ?>
+
+<br/>
+<br/>
+
+  </body>
+</html>
