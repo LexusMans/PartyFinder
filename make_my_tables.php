0ad2fa50b8241f299ca3716ff393a288993a51d9
LexusMans <lexus_mans@MacBookPro-C82A1425C4EC.local>
2015-05-15 09:26:08 -0500

    added supporting php scripts for registration

--- make_my_tables.php
+++ make_my_tables.php
@@ -6,7 +6,6 @@ $tbl_users = "CREATE TABLE IF NOT EXISTS users (
 			  username VARCHAR(16) NOT NULL,
 			  email VARCHAR(255) NOT NULL,
 			  password VARCHAR(255) NOT NULL,
-			  gender ENUM('m','f') NOT NULL,
 			  userlevel ENUM('a','b','c','d') NOT NULL DEFAULT 'a',
 			  avatar VARCHAR(255) NULL,
 			  ip VARCHAR(255) NOT NULL,
@@ -27,7 +26,6 @@ if ($query === TRUE) {
 $tbl_useroptions = "CREATE TABLE IF NOT EXISTS useroptions ( 
                 id INT(11) NOT NULL,
                 username VARCHAR(16) NOT NULL,
-				background VARCHAR(255) NOT NULL,
 				question VARCHAR(255) NULL,
 				answer VARCHAR(255) NULL,
                 PRIMARY KEY (id),
