186725f8e108b3273beeb7bbd431353cc70b2f30
LexusMans <lexus_mans@97-85-165-16.static.stls.mo.charter.com>
2015-05-15 23:29:51 -0500

    email credentials authentication

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 session_start();
 // If user is logged in, header them away
 if(isset($_SESSION["username"])){
-  header("location: message.php?msg=NO to that weenis");
+  header("location: message.php?msg=You are already logged in!");
     exit();
 }
 ?>
@@ -30,91 +30,102 @@ if(isset($_POST["usernamecheck"])){
       exit();
     }
 }
-?><?php
+?>
+<?php
 // Ajax calls this REGISTRATION code to execute
 if(isset($_POST["u"])){
-  // CONNECT TO THE DATABASE
-  include_once("php_includes/db_conx.php");
-  // GATHER THE POSTED DATA INTO LOCAL VARIABLES
-  $u = preg_replace('#[^a-z0-9]#i', '', $_POST['u']);
-  $e = mysqli_real_escape_string($db_conx, $_POST['e']);
-  $p = $_POST['p'];
-  //$g = preg_replace('#[^a-z]#', '', $_POST['g']);
-  //$c = preg_replace('#[^a-z ]#i', '', $_POST['c']);
-  // GET USER IP ADDRESS
-    $ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
-  // DUPLICATE DATA CHECKS FOR USERNAME AND EMAIL
-  $sql = "SELECT id FROM users WHERE username='$u' LIMIT 1";
-    $query = mysqli_query($db_conx, $sql); 
-  $u_check = mysqli_num_rows($query);
-  // -------------------------------------------
-  $sql = "SELECT id FROM users WHERE email='$e' LIMIT 1";
-    $query = mysqli_query($db_conx, $sql); 
-  $e_check = mysqli_num_rows($query);
-  // FORM DATA ERROR HANDLING
-  if($u == "" || $e == "" || $p == ""/* || $g == "" || $c == ""*/){
-    echo "The form submission is missing values.";
-        exit();
-  } else if ($u_check > 0){ 
-        echo "The username you entered is alreay taken";
-        exit();
-  } else if ($e_check > 0){ 
-        echo "That email address is already in use in the system";
-        exit();
-  } else if (strlen($u) < 3 || strlen($u) > 16) {
-        echo "Username must be between 3 and 16 characters";
-        exit(); 
-    } else if (is_numeric($u[0])) {
-        echo 'Username cannot begin with a number';
-        exit();
-    } else {
-  // END FORM DATA ERROR HANDLING
-    // Begin Insertion of data into the database
-    // Hash the password and apply your own mysterious unique salt
-    $cryptpass = crypt($p);
-    include_once ("php_includes/randStrGen.php");
-    $p_hash = randStrGen(20)."$cryptpass".randStrGen(20);
-    // Add user info into the database table for the main site table
-    //$sql = "INSERT INTO users (username, email, password, gender, country, ip, signup, lastlogin, notescheck) VALUES('$u','$e','$p_hash','$g','$c','$ip',now(),now(),now())";
-    $sql = "INSERT INTO users (username, email, password, ip, signup, lastlogin, notescheck) VALUES('$u','$e','$p_hash','$ip',now(),now(),now())";
-    $query = mysqli_query($db_conx, $sql); 
-    $uid = mysqli_insert_id($db_conx);
-    // Establish their row in the useroptions table
-    $sql = "INSERT INTO useroptions (id, username) VALUES ('$uid','$u')";
-    $query = mysqli_query($db_conx, $sql);
-    // Create directory(folder) to hold each user's files(pics, MP3s, etc.)
-    if (!file_exists("user/$u")) {
-      mkdir("user/$u", 0755);
-    }
-    // Email the user their activation link
-    $to = "$e";
-    $from = "lem346@mail.missouri.edu";
-    $subject = 'Party Finder Account Activation';
-    $message = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Party Finder Message</title></head><body style="margin:0px; font-family:Tahoma, Geneva, sans-serif;"><div style="padding:10px; background:#333; font-size:24px; color:#CCC;"><a href="http://babbage.cs.missouri.edu/~lem346/PartyFinder/index.php"></a>Party Finder Account Activation</div><div style="padding:24px; font-size:17px;">Hello '.$u.',<br /><br />Click the link below to activate your account when ready:<br /><br /><a href="https://http://babbage.cs.missouri.edu/~lem346/PartyFinder/activation.php?id='.$uid.'&u='.$u.'&e='.$e.'&p='.$p_hash.'">Click here to activate your account now</a><br /><br />Login after successful activation using your:<br />* E-mail Address: <b>'.$e.'</b></div></body></html>';
-    $headers = "From: $from\n";
-        $headers .= "MIME-Version: 1.0\n";
-        $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-    mail($to, $subject, $message, $headers);
-    echo "signup_success";
-    exit();
-  }
-  exit();
+	// CONNECT TO THE DATABASE
+	include_once("php_includes/db_conx.php");
+
+	// GATHER THE POSTED DATA INTO LOCAL VARIABLES
+	$u = preg_replace('#[^a-z0-9]#i', '', $_POST['u']);
+	$e = mysqli_real_escape_string($db_conx, $_POST['e']);
+	$p = $_POST['p'];
+	// GET USER IP ADDRESS
+	$ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
+	
+	// DUPLICATE DATA CHECKS FOR USERNAME AND EMAIL
+	$sql = "SELECT id FROM users WHERE username='$u' LIMIT 1";
+	$query = mysqli_query($db_conx, $sql); 
+	$u_check = mysqli_num_rows($query);
+	// -------------------------------------------
+	$sql = "SELECT id FROM users WHERE email='$e' LIMIT 1";
+	$query = mysqli_query($db_conx, $sql); 
+	$e_check = mysqli_num_rows($query);
+	// FORM DATA ERROR HANDLING
+	if($u == "" || $e == "" || $p == ""){
+		echo "The form submission is missing values.";
+			exit();
+	}
+	else if ($u_check > 0){ 
+		echo "The username you entered is alreay taken";
+		exit();
+	}
+	else if ($e_check > 0){ 
+		echo "That email address is already in use in the system";
+		exit();
+	}
+	else if (strlen($u) < 3 || strlen($u) > 16) {
+		echo "Username must be between 3 and 16 characters";
+		exit(); 
+	} else if (is_numeric($u[0])) {
+		echo 'Username cannot begin with a number';
+		exit();
+	}
+	else {
+		// END FORM DATA ERROR HANDLING
+		// Begin Insertion of data into the database
+		// Hash the password and apply your own mysterious unique salt
+		$cryptpass = crypt($p);
+		include_once ("php_includes/randStrGen.php");
+		$p_hash = randStrGen(20)."$cryptpass".randStrGen(20);
+		// Add user info into the database table for the main site table
+		$sql = "INSERT INTO users (username, email, password, ip, signup, lastlogin, notescheck) VALUES('$u','$e','$p_hash','$ip',now(),now(),now())";
+		//$query = mysqli_query($db_conx, $sql); 
+		if (mysqli_query($db_conx, $sql) != true){
+			echo "unsuccessful insertion";
+			exit();
+		} 
+		$uid = mysqli_insert_id($db_conx);
+		// Establish their row in the useroptions table
+		$sql = "INSERT INTO useroptions (id, username) VALUES ('$uid','$u')";
+		//$query = mysqli_query($db_conx, $sql);
+		if (mysqli_query($db_conx, $sql) != true){
+			echo "unsuccessful insertion 2";
+			exit();
+		} 
+		// Create directory(folder) to hold each user's files(pics, MP3s, etc.)
+		if (!file_exists("user/$u")) {
+		  mkdir("user/$u", 0755);
+		}
+		// Email the user their activation link
+		$to = "$e";
+		$from = "lem346@mail.missouri.edu";
+		$subject = 'Party Finder Account Activation';
+		$message = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Party Finder Message</title></head><body style="margin:0px; font-family:Tahoma, Geneva, sans-serif;"><div style="padding:10px; background:#333; font-size:24px; color:#CCC;"><a href="http://babbage.cs.missouri.edu/~lem346/PartyFinder/index.php"></a>Party Finder Account Activation</div><div style="padding:24px; font-size:17px;">Hello '.$u.',<br /><br />Click the link below to activate your account when ready:<br /><br /><a href="https://http://babbage.cs.missouri.edu/~lem346/PartyFinder/activation.php?id='.$uid.'&u='.$u.'&e='.$e.'&p='.$p_hash.'">Click here to activate your account now</a><br /><br />Login after successful activation using your:<br />* E-mail Address: <b>'.$e.'</b></div></body></html>';
+		$headers = "From: $from\n";
+		$headers .= "MIME-Version: 1.0\n";
+		$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+		mail($to, $subject, $message, $headers);
+		echo "signup_success";
+		exit();
+	}
+	exit();
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-	<style type="text/css">
-   		body { background: navy; }
-	</style>
-
+<html>
   <head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  
+  <script src="js/main.js"></script>
+  <script src="js/ajax.js"></script>
 
 <script>
 function restrict(elem){
@@ -144,101 +155,67 @@ function checkusername(){
   }
 }
 function signup(){
-  var u = _("username").value;
-  var e = _("email").value;
-  var p1 = _("pass1").value;
-  var p2 = _("pass2").value;
-  //var c = _("country").value;
-  //var g = _("gender").value;
-  var status = _("status");
-  if(u == "" || e == "" || p1 == "" || p2 == ""/* || c == "" || g == ""*/){
-    status.innerHTML = "Fill out all of the form data";
-  } else if(p1 != p2){
-    status.innerHTML = "Your password fields do not match";
-  } else {
-    _("signupbtn").style.display = "none";
-    status.innerHTML = 'please wait ...';
-    var ajax = ajaxObj("POST", "signup.php");
-        ajax.onreadystatechange = function() {
-          if(ajaxReturn(ajax) == true) {
-              if(ajax.responseText != "signup_success"){
-          status.innerHTML = ajax.responseText;
-          _("signupbtn").style.display = "block";
-        } else {
-          window.scrollTo(0,0);
-          _("signupform").innerHTML = "OK "+u+", check your email inbox and junk mail box at <u>"+e+"</u> in a moment to complete the sign up process by activating your account. You will not be able to do anything on the site until you successfully activate your account.";
-        }
-          }
-        }
-        ajax.send("u="+u+"&e="+e+"&p="+p1/*+"&c="+c+"&g="+g*/);
-  }
+	var u = _("username").value;
+	var e = _("email").value;
+	var p1 = _("pass1").value;
+	var p2 = _("pass2").value;
+	var status = _("status");
+	if(u == "" || e == "" || p1 == "" || p2 == ""){
+		status.innerHTML = "Fill out all of the form data";
+	}
+	else if(p1 != p2){
+		status.innerHTML = "Your password fields do not match";
+	}
+	else {
+		_("signupbtn").style.display = "none";
+		status.innerHTML = 'please wait ...';
+		var ajax = ajaxObj("POST", "signup.php");
+		ajax.onreadystatechange = function() {
+			if(ajaxReturn(ajax) == true) {
+				  if(ajax.responseText != "signup_success"){
+					status.innerHTML = ajax.responseText;
+					_("signupbtn").style.display = "block";
+				  } else {
+					window.scrollTo(0,0);
+					_("signupform").innerHTML = "OK "+u+", check your email inbox and junk mail box at <u>"+e+"</u> in a moment to complete the sign up process by activating your account. You will not be able to do anything on the site until you successfully activate your account.";
+				  }
+			}
+			else{
+				status.innerHTML = "ajaxReturn(ajax) != true";
+				_("signupbtn").style.display = "block";
+			}
+			ajax.send("u="+u+"&e="+e+"&p="+p1);
+		}
+	}
 }
-/*
-function openTerms(){
-  _("terms").style.display = "block";
-  emptyElement("status");
-}
-*/
-/* function addEvents(){
-  _("elemID").addEventListener("click", func, false);
-}
-window.onload = addEvents; */
 </script>
-  </head>
-  <body>
-  	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 
-	<?php include_once("template_navbar.php"); ?>
+<?php include_once("template_navbar.php"); ?>
 
 <br/>
 <br/>
 
-<div id="pageMiddle">
   <h3>Sign Up Here</h3>
-  <form name="signupform" id="signupform" onsubmit="return false;">
-    <div>Username: </div>
-    <input id="username" type="text" onblur="checkusername()" onkeyup="restrict('username')" maxlength="16">
-    <span id="unamestatus"></span>
-    <div>Email Address:</div>
-    <input id="email" type="text" onfocus="emptyElement('status')" onkeyup="restrict('email')" maxlength="88">
-    <div>Create Password:</div>
-    <input id="pass1" type="password" onfocus="emptyElement('status')" maxlength="16">
-    <div>Confirm Password:</div>
-    <input id="pass2" type="password" onfocus="emptyElement('status')" maxlength="16">
-    <!--
-    <div>Gender:</div>
-    <select id="gender" onfocus="emptyElement('status')">
-      <option value=""></option>
-      <option value="m">Male</option>
-      <option value="f">Female</option>
-    </select>
-    <div>Country:</div>
-    <select id="country" onfocus="emptyElement('status')">
-      <?php //include_once("template_country_list.php"); ?>
-    </select>
-    -->
-    <!--
-    <div>
-      <a href="#" onclick="return false" onmousedown="openTerms()">
-        View the Terms Of Use
-      </a>
-    </div>
-    <div id="terms" style="display:none;">
-      <h3>Web Intersect Terms Of Use</h3>
-      <p>1. Play nice here.</p>
-      <p>2. Take a bath before you visit.</p>
-      <p>3. Brush your teeth before bed.</p>
-    </div>
-    -->
-    <br /><br />
-    <button id="signupbtn" onclick="signup()">Create Account</button>
-    <span id="status"></span>
-  </form>
-</div>
-
-
-  </body>
+	<form role="form" name=signupform" id="signupform" onsubmit="return false">
+		<div>Username: </div>
+		<input id="username" type="text" onblur="checkusername()" maxlength="16">
+		<span id="unamestatus"></span>
+		<div>Email Address:</div>
+		<input id="email" type="text" maxlength="88">
+		<div>Create Password:</div>
+		<input id="pass1" type="password" maxlength="16">
+		<div>Confirm Password:</div>
+		<input id="pass2" type="password" maxlength="16">
+		<br /><br />
+		<button id="signupbtn" onclick="signup()">Create Account</button>
+		</br></br>
+		<span id="status">Good</span>
+	</form>
+</body>
 </html>
